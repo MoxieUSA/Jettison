66a45665f3165a507b22f37b3c3be9df7502ea4b
Justin Langston <jll2500@gmail.com>
2011-11-22 11:51:26 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var pub = __dirname + '/public';
+
+var app = express.createServer();
+app.use(express.compiler({ src: pub, enable: ['less', 'sass', 'coffeescript']}));
+app.use(app.router);
+app.use(express.static(pub));
+app.use(express.errorHandler({ dump: true, stack: true }));
+app.set('views', __dirname + '/views');
+app.register('.hbs', require('hbs'));
+app.set('view engine', 'hbs');
+app.set('view options', {layout: true});
+
+app.get('/', function(req, res){
+	res.render('index');
+});
+
+app.listen(3000);
