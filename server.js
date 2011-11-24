3bdb9310a8a5b5725cbcd374940291bcc6cb25c0
Justin Langston <jll2500@gmail.com>
2011-11-24 04:17:51 +0000

    Adding package file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var pub = __dirname + '/public';
+var gzippo = require('gzippo');
 
 var app = express.createServer();
 app.use(express.compiler({ src: pub, enable: ['less', 'sass', 'coffeescript']}));
 app.use(app.router);
-app.use(express.static(pub));
+app.use(gzippo.staticGzip(pub));
 app.use(express.errorHandler({ dump: true, stack: true }));
 app.set('views', __dirname + '/views');
 app.register('.hbs', require('hbs'));
@@ -14,5 +15,5 @@ app.set('view options', {layout: true});
 app.get('/', function(req, res){
 	res.render('index');
 });
-
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
