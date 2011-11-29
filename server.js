6a0027053a44abafc17c297d22ac3a84da939c5a
Justin Langston <jll2500@gmail.com>
2011-11-29 18:26:45 +0000

    prettying

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 var express = require('express');
 var pub = __dirname + '/public';
-
 var app = express.createServer();
-app.use(express.compiler({ src: pub, enable: ['less', 'sass', 'coffeescript']}));
+app.use(express.compiler({
+    src: pub,
+    enable: ['less', 'sass', 'coffeescript']
+}));
 app.use(app.router);
 app.use(express.static(pub));
-app.use(express.errorHandler({ dump: true, stack: true }));
+app.use(express.errorHandler({
+    dump: true,
+    stack: true
+}));
 app.set('views', __dirname + '/views');
 app.register('.hbs', require('hbs'));
 app.set('view engine', 'hbs');
-app.set('view options', {layout: true});
-
-app.get('/', function(req, res){
-	res.render('index');
+app.set('view options', {
+    layout: true
+});
+app.get('/', function(req, res) {
+    res.render('index');
 });
 var port = process.env.PORT || 3000;
 app.listen(port);
